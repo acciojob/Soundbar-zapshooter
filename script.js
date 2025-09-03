e63a41fb2514c5505ec6735b2c6413146ac81514
zapshooter <92568875+zapshooter@users.noreply.github.com>
2025-09-03 20:57:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong'];
+
+const buttons = document.querySelectorAll('.btn');
+let currentAudio = null;
+
+// Play sound
+buttons.forEach(btn => {
+	btn.addEventListener('click', () => {
+		const soundName = btn.textContent.trim();
+		if (soundName === 'stop') {
+			if (currentAudio) {
+				currentAudio.pause();
+				currentAudio.currentTime = 0;
+			}
+			return;
+		}
+		if (currentAudio) {
+			currentAudio.pause();
+			currentAudio.currentTime = 0;
+		}
+		currentAudio = new Audio(`./sounds/${soundName}.mp3`);
+		currentAudio.play();
+	});
+});
